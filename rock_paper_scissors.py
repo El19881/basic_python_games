bf57bab32e7b4e9a3aa2c857f00de198852884f6
Ariel Ferensztajn <77733595+El19881@users.noreply.github.com>
2023-09-18 17:52:59 +0200

    Update rock_paper_scissors.py
    Finalized version with message of the winner of the game.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,14 +1,3 @@
-#ROCK PAPER SCISORR NOW
-
-#random number to select rock/paper/scisor for computer
-#input from user for selection
-#make sure there is error handling for user input
-#display info who lost
-#best of 3 length (add a counter)
-#display overall winner
-# ask if user wants to play again
-#error handling for user input
-
 import random
 
 print("Welcome to rock, paper, scissors game!\n You will need to provide your choice of rock, paper or scissors and compete with the program. Best of 3 wins, good luck!")
@@ -16,18 +5,15 @@ print("Welcome to rock, paper, scissors game!\n You will need to provide your ch
 def user_select():
 	user_selection = input("Provide your selection. Type:\n 1: Rock\n 2: Paper\n 3: Scissors\n Your selection: ")
 	return user_selection
-	#if (user_selection != "1") or (user_selection != "2") or (user_selection != "3"):
-		#print("You did not provide a valid vaue. Please try again.")
-		#return user_select()
 		
 def computer_select():
 	computer_selection = random.randrange(1,3)
 	return computer_selection
-#NEED TO FIGURE OUT ERROR HANDLING WHEN WRONG INPUT GIVEN
+
 def main_loop():
 	computer_wins = 0
 	player_wins = 0
-	while (computer_wins < 2) and (player_wins < 2):
+	while (computer_wins != 3) and (player_wins < 3):
 		computer_selection = computer_select()
 		user_selection = user_select()
 		if (user_selection != "1") and (user_selection != "2") and (user_selection != "3"):
@@ -56,5 +42,10 @@ def main_loop():
 		elif computer_selection == 3 and user_selection == "2":
 			print("Program won! Scissors beat paper")
 			computer_wins += 1
+	if computer_wins == 3:
+		print("The computer won the game with 3 wins!")
+	elif player_wins == 3:
+		print("The player won the game with 3 wins!")
+	
 
 main_loop()
