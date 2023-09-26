7450a63a953ec5c92bd6c021327cf652fefecc44
Ariel Ferensztajn <77733595+El19881@users.noreply.github.com>
2023-09-26 20:23:49 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,14 +1,3 @@
-#player1 input word
-#player2 input letter
-#check if letter in word
-#if not add to the hangman
-#display wrong letters and correct letters
-#board for good letters?
-#hangman build:multiline string?
-# add .upper() zeby zawsze patrzec na to samo niezaleznie od kapitalizacji
-# add logic for word input to check validity (no spaces or dashes in the word)
-
-
 def hangman():
 	guess_word_input = input("Please provide the word the player will need to guess: ")
 	word_letters = set(guess_word_input) #letters in the word to guess
@@ -151,11 +140,10 @@ def hangman():
 """]
 
 	
-	#getting user input
+	
 	while len(word_letters) > 0 and hangman_status != 11:
-		#letters used
+		
 		print("You have used so far: ", ','.join(provided_letters))
-		#what is the current status of letters guessed with dashes: W-O-G (WRONG)
 		word_list = [letter if letter in provided_letters else "-" for letter in guess_word_input]
 		print("The word to guess is: ", ' '.join(word_list))
 
@@ -172,10 +160,8 @@ def hangman():
 				
 		elif player_guess_letter in provided_letters:
 			print("You already used this letter. Try a different one.")
-		#	return player_letter()
 		elif player_guess_letter not in letters:
 			print("This is not a valid letter. Please try again.")
-		#	return player_letter
 
 	if hangman_status == 11:
 		print("You lost!")
@@ -194,6 +180,3 @@ def play_again():
 
 
 hangman()
-#play_again()
-
-
